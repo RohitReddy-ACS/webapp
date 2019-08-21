f623fde6aa7160c7d20cc96dbd0e6c41215d8d9a
RohitReddy-ACS <rreddy@rreddym011047.mayo.edu>
2019-08-21 09:29:23 -0500

    adding webapp files

---
+++ Startup.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace aspnetcoreapp
+{
+    public class Startup
+    {
+
+        private readonly ILogger _logger;
+
+        public Startup(IConfiguration configuration, ILoggerFactory logFactory)
+        {
+            _logger = logFactory.CreateLogger<Startup>();
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            _logger.LogInformation("INIT: In ConfigureServices() method");
+            _logger.LogInformation("INIT: Running on instance <" + Environment.GetEnvironmentVariable("COMPUTERNAME") + ">");
+            _logger.LogInformation("INIT: Running slot is <" + Environment.GetEnvironmentVariable("HTTP_HOST") + ">");
+            Trace.TraceInformation("INIT: Running on instance <" + Environment.GetEnvironmentVariable("COMPUTERNAME") + ">"); // Write an information message
+            Trace.TraceInformation("INIT: Running slot is <" + Environment.GetEnvironmentVariable("HTTP_HOST") + ">");
+
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc();
+        }
+    }
+}
